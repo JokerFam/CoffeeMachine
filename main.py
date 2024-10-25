de954c5b7956128d8e317406bac9d05937b2495d
mq mala <malandula@icloud.com>
2024-10-25 12:50:36 -0600

    main file is updated to run code

--- main.py
+++ main.py
@@ -3,10 +3,7 @@ from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
 
 
-def whatDrinkYouWant():
-    print(Menu().get_items())
-    userInput = str(input('What drink do you want? \n'))
-
+def whatDrinkYouWant(userInput):
     if CoffeeMaker().is_resource_sufficient(Menu().find_drink(userInput)) == True:
         if MoneyMachine().make_payment(Menu().find_drink('latte').cost) == True:
             CoffeeMaker().make_coffee(Menu().find_drink(userInput))
@@ -17,8 +14,23 @@ def whatDrinkYouWant():
             Menu().find_drink(userInput)))
 
 
+def reporting():
+    CoffeeMaker().report()
+    MoneyMachine().report()
+
+
 def main():
-    print()
+    itsOn = 'on'
+    while itsOn != 'off':
+        print(Menu().get_items())
+        userInput = str(input('What drink do you want? \n'))
+
+        if userInput.lower() == 'off':
+            itsOn = 'off'
+        elif userInput.lower() == 'report':
+            reporting()
+        else:
+            whatDrinkYouWant(userInput)
 
 
 if __name__ == "__main__":
